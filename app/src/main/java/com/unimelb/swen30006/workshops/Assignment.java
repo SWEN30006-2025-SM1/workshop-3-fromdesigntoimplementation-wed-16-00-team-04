531fca94b6cf911a370f95bf6e3f1c57cb694350
Himesh Sonal Weerawardane <hweerawardan@student.unimelb.edu.au>
2025-03-26 17:54:08 +1100

    Done with the first level of work

--- app/src/main/java/com/unimelb/swen30006/workshops/Assignment.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Assignment.java
@@ -1,4 +1,24 @@
 package com.unimelb.swen30006.workshops;
 
 public class Assignment {
+    String description;
+    String dueDate;
+    String name;
+    int maxAttempts;
+
+    public Assignment (String assignmentDescription, String assignmentDueDate, String assignmentName, int assignmentMaxAttempts) {
+        description = assignmentDescription;
+        dueDate = assignmentDueDate;
+        name = assignmentName;
+        maxAttempts = assignmentMaxAttempts;
+    }
+
+    public Submission[] invalidSubmission() {
+        ;
+    }
+
+    public Submission[] validSubmission() {
+        ;
+    }
+
 }
--- app/src/main/java/com/unimelb/swen30006/workshops/Subject.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Subject.java
@@ -1,4 +1,20 @@
 package com.unimelb.swen30006.workshops;
 
 public class Subject {
+
+    String subjectName;
+    String subjectCode;
+
+    public Subject(String name, String code) {
+        subjectName = name;
+        subjectCode = code;
+    }
+
+    public void createAssignment() {
+        System.out.println("Assignment Created for " + subjectName + ", " + subjectCode);
+    }
+
+    public void deleteAssignment(String assignmentName) {
+        System.out.println("Assignment '" + assignmentName + "' has been deleted, for " + subjectName + ", " + subjectCode);
+    }
 }
