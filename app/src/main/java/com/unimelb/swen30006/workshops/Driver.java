4fae918417eeb095899bfb48b3cfe9b1adf61213
hhuppatz <harryhuppatz@gmail.com>
2025-03-27 10:42:54 +1100

    Started working on Driver.java

---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Driver.java
@@ -0,0 +1,9 @@
+package com.unimelb.swen30006.workshops;
+
+public class Driver {
+    private static SampleValidator sv;
+    public static void main(String[] args)
+    {
+        sv.validateSubmission(Submission);
+    }
+}
